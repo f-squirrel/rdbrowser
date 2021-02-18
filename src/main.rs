f06a57e1d821b193623f4b087e6678127d4bd844
f-squirrel <dmitry.b.danilov@gmail.com>
2021-02-18 19:52:52 +0200

    Added rocksdb and clap
    * rocksdb - rust binding to RocksDB
    * clap - command line parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+use clap::{App, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rdBrowser")
+        .version("0.1")
+        .author("Dmitry Danilov")
+        .about("CLI Browser for RocksDB\nhttps://github.com/f-squirrel/rdbrowser")
+        .arg(
+            Arg::with_name("db")
+                .long("db")
+                .value_name("FILE")
+                .help("Sets path to the target database")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
+    println!("Using input file: {}", matches.value_of("db").unwrap());
 }

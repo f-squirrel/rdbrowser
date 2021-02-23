daaa301cc191401bfd883ac4be2cb4228867a2e7
f-squirrel <dmitry.b.danilov@gmail.com>
2021-02-23 19:29:25 +0200

    Gracefully close DB before exit(1)

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let cmd = create(cmd_parser::get_cmd_args());
     if let Err(_error) = cmd.run() {
         println!("Failed: {}", _error);
+        drop(cmd);
         std::process::exit(1);
     }
 }

8e12abcc840fab34b72ae8fcf48b0afd28888bfb
f-squirrel <dmitry.b.danilov@gmail.com>
2021-03-14 20:00:24 +0200

    Remove redundant struct member

--- src/command/checkconsistency.rs
+++ src/command/checkconsistency.rs
@@ -5,21 +5,19 @@ use std::boxed::Box;
 use std::error::Error;
 
 #[derive(Debug)]
-pub struct CheckConsistency {
-    db: DB,
-}
+pub struct CheckConsistency {}
 
 impl<'a> CheckConsistency {
     pub fn create(matches: &'a ArgMatches<'a>) -> Result<Box<dyn Command + 'a>, Box<dyn Error>> {
         let mut opts = Self::build_options(matches);
         opts.set_paranoid_checks(true);
         opts.set_num_levels(64);
-        let db = DB::open_cf(
+        DB::open_cf(
             &opts,
             matches.value_of("db").unwrap(),
             &[matches.value_of("column_family").unwrap()],
         )?;
-        Ok(std::boxed::Box::new(CheckConsistency { db }))
+        Ok(std::boxed::Box::new(CheckConsistency {}))
     }
 }
 
